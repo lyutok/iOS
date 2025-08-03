6e4eae91e75f835492f793b19e96bae086dd674e
lyutok <48644325+lyutok@users.noreply.github.com>
2025-08-04 00:41:00 +0300

    Add EggTimer

---
+++ Quizler/Quizler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizler
+//
+//  Created by Lyudmila Tokar on 8/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
