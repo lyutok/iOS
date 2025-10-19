14b3666569f43864cbd6003c783e8cf108aacdf7
lyutok <48644325+lyutok@users.noreply.github.com>
2025-10-19 01:28:46 +0100

    Change srcreens, add headers

---
+++ ShareDemo/ShareDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShareDemo
+//
+//  Created by Lyudmila Tokar on 10/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShareDemo/ShareDemo/ShareDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareDemoApp.swift
+//  ShareDemo
+//
+//  Created by Lyudmila Tokar on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ShareDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
